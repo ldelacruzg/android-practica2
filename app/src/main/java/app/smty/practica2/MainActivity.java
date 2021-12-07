14d0f5950790aca3f9c77135c43bf7041ace3f21
delacruz <ldelacruzg@uteq.edu.ec>
2021-12-07 12:34:37 -0500

    Separando la logica de Retrofit de MainActivity

--- app/src/main/java/app/smty/practica2/MainActivity.java
+++ app/src/main/java/app/smty/practica2/MainActivity.java
@@ -2,49 +2,26 @@ package app.smty.practica2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
 
-import java.util.List;
-
-import app.smty.practica2.Models.GorestResponseGeneral;
-import app.smty.practica2.Services.GorestService;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
-    TextView textViewUsers;
-    String result = "";
+    Button buttonRetrofit, ButtonVolley;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textViewUsers = findViewById(R.id.textViewUsers);
+        buttonRetrofit = findViewById(R.id.buttonRetrofit);
 
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://gorest.co.in/public/v1/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-
-        GorestService service = retrofit.create(GorestService.class);
-        Call<GorestResponseGeneral> users = service.getUsers();
-        users.enqueue(new Callback<GorestResponseGeneral>() {
+        buttonRetrofit.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onResponse(Call<GorestResponseGeneral> call, Response<GorestResponseGeneral> response) {
-                GorestResponseGeneral data = response.body();
-                data.getUserList().forEach(user -> {
-                    result += user.toString() + "\n\n";
-                });
-                textViewUsers.setText(result);
-            }
-
-            @Override
-            public void onFailure(Call<GorestResponseGeneral> call, Throwable t) {
-
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RetrofitActivity.class);
+                startActivity(intent);
             }
         });
     }
